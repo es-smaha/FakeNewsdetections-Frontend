c0eb338413b5ee036b2f80dec9397012ee723490
es-smaha <samahaoum14@gmail.com>
2021-06-25 11:58:39 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
-import { NgModule } from '@angular/core';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { SearchComponent } from './search/search.component';
+import { NewsComponent } from './news/news.component';
+import { TextComponent } from './text/text.component';
+import { NltkComponent } from './nltk/nltk.component';
+
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path : 'nltk' , component:NltkComponent},
+  { path : 'texttest' , component:TextComponent},
+  { path : 'news' , component:NewsComponent},
+  { path : 'search' , component:SearchComponent},
+  { path : 'aboutus' , component:AboutusComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,54 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {HttpClientModule} from '@angular/common/http';
+import { GraphQLModule } from './graphql.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { TextComponent } from './text/text.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import { NltkComponent } from './nltk/nltk.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { NewsComponent } from './news/news.component';
+import {NgxPaginationModule} from 'ngx-pagination'; 
+import { ElasticsearchService } from './elasticsearch.service';
+import { SearchComponent } from './search/search.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TextComponent,
+    NavbarComponent,
+     NltkComponent,
+    NewsComponent,
+    SearchComponent,
+  
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    GraphQLModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgbModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    NgxPaginationModule
   ],
-  providers: [],
+  providers: [ElasticsearchService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+ 
+
+ }
